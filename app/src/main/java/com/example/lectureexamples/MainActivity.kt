774ca04163babb545fffba623e43e5ba9c346cca
leonardo1710 <leon.freudenthaler@fh-campuswien.ac.at>
2023-03-13 16:21:17 +0100

    Prepared repo for navigation lecture

--- app/src/main/java/com/example/lectureexamples/MainActivity.kt
+++ app/src/main/java/com/example/lectureexamples/MainActivity.kt
@@ -1,23 +1,29 @@
 package com.example.lectureexamples
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.lectureexamples.models.Movie
+import com.example.lectureexamples.models.getMovies
 import com.example.lectureexamples.ui.theme.LectureExamplesTheme
 
 class MainActivity : ComponentActivity() {
@@ -51,30 +57,61 @@ class MainActivity : ComponentActivity() {
 
 @Preview
 @Composable
-fun MyList(messages: List<String> = listOf("hello", "there")){
+fun MyList(movies: List<Movie> = getMovies()){
 
     LazyColumn{
 
-        items(messages) {message ->
-            Text(
-                text = message,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .border(2.dp, color = Color.Cyan)
-                    .padding(10.dp)
-                    .height(100.dp)
-                    .clickable {
-                    Log.d("MyList", "item clicked")
-                }
-            )
+        items(movies) {movie ->
+            MovieRow(movie = movie.title)
         }
     }
-    
 }
 
 
+@Composable
+fun MovieRow(movie: String) {
+    Card(modifier = Modifier
+        .fillMaxWidth()
+        .padding(5.dp),
+        shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+        elevation = 5.dp
+    ) {
+        Column {
+            Box(modifier = Modifier
+                .height(150.dp)
+                .fillMaxWidth()
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.avatar2),
+                    contentDescription = "Movie Poster",
+                    contentScale = ContentScale.Crop
+                )
 
+                Box(modifier = Modifier
+                    .fillMaxSize()
+                    .padding(10.dp),
+                    contentAlignment = Alignment.TopEnd
+                ){
+                    Icon(
+                        tint = MaterialTheme.colors.secondary,
+                        imageVector = Icons.Default.FavoriteBorder,
+                        contentDescription = "Add to favorites")
+                }
+            }
 
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(movie, style = MaterialTheme.typography.h6)
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowUp,
+                    contentDescription = "Show details")
+            }
+        }
+    }
+}
 
 @Preview
 @Composable
@@ -91,13 +128,6 @@ fun WelcomeText(modifier: Modifier = Modifier, text: String = "default") {
 
 }
 
-
-
-
-
-
-
-@SuppressLint("UnrememberedMutableState")
 @Preview
 @Composable
 fun Greeting() {
